eaa591aa2ba9e19c4daca669813823fc10420a72
Yingqin SU <swsuyingqin@gmail.com>
2021-04-29 22:53:45 +0200

    ajout de la première version du design

--- ricochet_Adel_Jonathan_Yassine_Djibril_Mohamed_Yingqin.py
+++ ricochet_Adel_Jonathan_Yassine_Djibril_Mohamed_Yingqin.py
@@ -20,16 +20,63 @@ COUL_FOND_2 = "#1b1b1b"
 COUL_FOND_3 = "#292929"
 COUL_QUADR = "#99958d"
 COTE = 47
+charger = False
 
 liste_obstacles = ["2", "3", "R", "B", "Y"]
 bot_controlling = "G"
-objet = ((11, 1), 'Y')
+
+map_initial = [
+    (7, 13),
+    (5, 0),
+    (4, 6),
+    (8, 2),
+    ((11, 1), "Y"),
+    [
+        "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
+        "201010301010101010101010301010102",
+        "*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*1*",
+        "201010101030101010101010103010102",
+        "*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*1*",
+        "201010101010101030103010101010102",
+        "*1*1*1*1*1*1*1*3*1*3*1*1*1*1*1*1*",
+        "201010101010101010101030101010102",
+        "*3*1*1*1*1*1*1*1*1*1*1*3*1*1*1*3*",
+        "2010101030101B1010101010101010102",
+        "*1*1*1*3*1*1*3*1*1*1*1*1*1*1*1*1*",
+        "2G1010101010301010101010101030102",
+        "*1*3*1*1*1*1*1*1*1*1*1*3*1*1*3*1*",
+        "201030101010101010101010301010102",
+        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+        "201010101010103030301010101R10102",
+        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+        "20101Y101010103030301010101010102",
+        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+        "201010101010101010101010101010102",
+        "*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*3*",
+        "201010101030101010301010101010102",
+        "*1*3*1*1*1*3*1*1*3*1*1*1*1*3*1*1*",
+        "203010101010101010101010103010102",
+        "*1*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*",
+        "201010101010103010101010101010102",
+        "*1*1*1*1*1*1*1*1*1*3*1*1*1*1*1*1*",
+        "201010101010101010103010101010102",
+        "*3*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*",
+        "201010103010101010101010101010302",
+        "*1*1*1*3*1*1*1*1*1*1*1*1*1*1*1*1*",
+        "201010101010301010101010301010102",
+        "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
+    ]
+]
+
+
+objet = map_initial[4]
 success = False
 
 liste = []
 with open("map.txt") as f:
     for line in f.readlines():
         liste.append(list(line[:-1]))
+
 
 def creer_objet(position, color):
     if color == "R":
@@ -42,12 +89,15 @@ def creer_objet(position, color):
         color = "yellow"
     return canvas.create_rectangle(58 + COTE * position[1], 58 + COTE * position[0], 70 + COTE * position[1], 70 + COTE * position[0], fill=color)
 
+
 def success_or_not(info_bot_controlled):
     global success
     if info_bot_controlled[1] == objet[0] and objet[1] == bot_controlling and success == False:
-        tkinter.messagebox.showinfo("success", "SUccess unlocked")
-        tkinter.messagebox.askquestion("Restart Request", 'Do you wanna restart?')
         success = True
+        canvas.unbind_all("<Key>")
+        tkinter.messagebox.showinfo("success", "SUccess unlocked")
+        tkinter.messagebox.askquestion("Restart Request", 'Do you wanna restart?')  
+
 
 def dessiner_obstacles():
     for i in range(len(liste)):
@@ -67,7 +117,7 @@ def dessiner_obstacles():
                     )
     
 
-def affiche_liste(liste):
+def affiche_liste(liste): # for test
     for sous_liste in liste:
         for element in sous_liste:
             print(element, end=" ")
@@ -283,29 +333,78 @@ def mouvement_right(bot_controlled, info_bot_controlled):
     return info_bot_controlled
 
 
-root = tk.Tk()
-root.title('Robot Ricochet')
-root.geometry(f"{LARGEUR+10}x{HAUTEUR+10}")
-root.configure(bg=COUL_FOND_2)
-root.resizable(False, False)
-
-canvas = tk.Canvas(height=HAUTEUR, width=LARGEUR, bg=COUL_FOND_2)
-canvas.grid()
-canvas.create_rectangle(33, 33, 801, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
-canvas.create_rectangle(41, 41, 793, 793, fill=COUL_FOND)
-quadrillage()
-
-canvas.create_rectangle(830, 33, 1160, 298, fill=COUL_FOND_3, outline=COUL_FOND_3)
-canvas.create_rectangle(830, 340, 925, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
-canvas.create_rectangle(947, 340, 1043, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
-canvas.create_rectangle(1065, 340, 1160, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
-canvas.create_rectangle(830, 441, 1160, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
+def charger_map():
+    pass
 
 
-info_bot_red = calculer_position(7, 13) + ["red"]
-info_bot_green = calculer_position(5, 0) + ["green"]
-info_bot_blue = calculer_position(4, 6) + ["blue"]
-info_bot_yellow = calculer_position(8, 2) + ["green"]
+def init():
+    global root, canvas
+    root = tk.Tk()
+    root.title('Robot Ricochet')
+    root.geometry(f"{LARGEUR+10}x{HAUTEUR+10}")
+    root.configure(bg=COUL_FOND_2)
+    root.resizable(False, False)
+
+    canvas = tk.Canvas(height=HAUTEUR, width=LARGEUR, bg=COUL_FOND_2)
+    canvas.grid()
+    canvas.create_rectangle(33, 33, 801, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
+    canvas.create_rectangle(41, 41, 793, 793, fill=COUL_FOND)
+    quadrillage()
+
+    canvas.create_rectangle(830, 33, 1160, 298, fill=COUL_FOND_3, outline=COUL_FOND_3)
+    canvas.create_rectangle(830, 340, 925, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
+    canvas.create_rectangle(947, 340, 1043, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
+    canvas.create_rectangle(1065, 340, 1160, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
+    canvas.create_rectangle(830, 441, 1160, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
+
+        ##rond de couleur
+    canvas.create_oval(864, 717, 914, 767, fill= 'green')
+    canvas.create_oval(864, 647, 914, 697, fill= 'blue')
+    canvas.create_oval(1001, 717, 1051, 767, fill= 'yellow')
+    canvas.create_oval(1001, 647, 1051, 697, fill= 'red')
+
+    ##Lettres
+    canvas.create_text(889, 742, fill= 'white', text='G',font = 'ArcadeClassic 30')
+    canvas.create_text(889, 672, fill= 'white', text='B',font = 'ArcadeClassic 30')
+    canvas.create_text(1026, 742, fill= '#9FAB18', text='Y',font = 'ArcadeClassic 30')
+    canvas.create_text(1026, 672, fill= 'white', text='R',font = 'ArcadeClassic 30')
+
+    ##Texte des couleur
+    canvas.create_text(949, 742, fill= 'white', text='Green',font = 'ArcadeClassic 17')
+    canvas.create_text(949, 672, fill= 'white', text='Blue',font = 'ArcadeClassic 17')
+    canvas.create_text(1086, 742, fill= 'white', text='Yellow',font = 'ArcadeClassic 17')
+    canvas.create_text(1086, 672, fill= 'white', text='Red',font = 'ArcadeClassic 17')
+
+    ##rectangle orange
+    canvas.create_rectangle(1001, 57.5, 1125, 117.5, fill="orange", outline='yellow')
+    canvas.create_rectangle(1001, 140, 1125, 200, fill='orange', outline='yellow')
+    canvas.create_rectangle(1001, 222.5, 1125, 282.5, fill='orange', outline='yellow')
+
+
+    ##Instruction
+    canvas.create_text(940, 595, fill= 'white', text='SWITCH ROBOT',font = 'ArcadeClassic 20')
+    canvas.create_text(935, 470, fill= 'white', text='HOW TO MOVE',font = 'ArcadeClassic 20')
+    #record
+    canvas.create_text(915, 85, fill= 'white', text='RECORD:',font = 'ArcadeClassic 20')
+    canvas.create_text(1062.5, 85, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+    #score
+    canvas.create_text(920, 167.5, fill= 'white', text='SCORE:',font = 'ArcadeClassic 20')
+    canvas.create_text(1062.5, 167.5, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+    #move
+    canvas.create_text(925, 250, fill= 'white', text='MOVE:',font = 'ArcadeClassic 20')
+    canvas.create_text(1062.5, 250, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+
+    dessiner_obstacles()
+    creer_objet(*objet)
+
+    canvas.bind_all("<Key>", deplacement)
+
+
+init()
+info_bot_red = calculer_position(*map_initial[0]) + ["red"]
+info_bot_green = calculer_position(*map_initial[1]) + ["green"]
+info_bot_blue = calculer_position(*map_initial[2]) + ["blue"]
+info_bot_yellow = calculer_position(*map_initial[3]) + ["green"]
 
 
 bot_red = create_rectangle_en_fonction_de_position(info_bot_red[1], "red")
@@ -313,10 +412,5 @@ bot_green = create_rectangle_en_fonction_de_position(info_bot_green[1], "green")
 bot_blue = create_rectangle_en_fonction_de_position(info_bot_blue[1], "blue")
 bot_yellow = create_rectangle_en_fonction_de_position(info_bot_yellow[1], "yellow")
 
-
-dessiner_obstacles()
-creer_objet(objet[0], objet[1])
-
-canvas.bind_all("<Key>", deplacement)
 
 root.mainloop()
