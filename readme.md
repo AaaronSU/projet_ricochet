dbf55697b157ee9dbf68c8d0ee214cc610c02318
Yingqin SU <swsuyingqin@gmail.com>
2021-05-01 10:01:39 +0200

    version 0.3 : intéraction avec un map txt

--- readme.md
+++ readme.md
@@ -1,1 +1,61 @@
-![](exemple de terrain.png)
+![](exemple_de_terrain.png)
+
+
+## Description du jeu Robot Ricochet
+
+Ce jeu est composé d’une grille carré de taille 16x16. Deux cases voisines de la grille peuvent être séparées par un mur vertical ou horizontal. De plus, on considère qu’un mur encadre le bord extérieur de la grille. Quatre robots de couleur rouge, jaune, vert et bleu sont placés sur des cases de la grille. Une case de la grille est de la couleur d’un des robots: il s’agit de la cible qui doit être atteinte par le robot de cette couleur afin de résoudre le jeu.
+
+Les règles de déplacement des robots sont les suivantes:
+
+- [X] ils ne se déplacent qu’en ligne droite, horizontale ou verticale
+
+- [X] ils ne s’arrêtent que quand ils rencontrent un obstacle qui est soit un mur soit un autre robot
+
+- [ ] cela compte pour un déplacement de robot, indépendamment du nombre de cases parcourues
+
+- [X] un seul robot se déplace à chaque fois
+
+Un objectif secondaire du jeu est de le résoudre en faisant le moins de déplacements possible.
+
+## Interface graphique
+
+Pour l’interface graphique, les principales caractéristiques attendues sont:
+
+- [X] les quatre robots sont représentés par des cercles de couleur rouge, jaune, vert et bleu
+
+- [X] la cible est représentée par un carré de la couleur d’un robot
+
+- [X] les quatre cases du milieu sont entourées de murs, et non accessibles par les robots; par ailleurs, un clic sur une de ces cases redémarre la partie au début
+
+- [X] quand on clique sur un robot, on peut ensuite le déplacer avec les flèches du clavier
+
+- [X] quand on clique sur les touches, on peut changer le robot
+
+- [ ] un compteur affiche le nombre de déplacements effectués
+
+- [ ] quand la cible est atteinte par le robot de la bonne couleur, un message affiche que le jeu est résolu et indique le score (le nombre de déplacements de robots)
+
+
+## Fonctionnalités avancées
+
+En plus de la programmation du jeu, vous programmerez les fonctionnalités suivantes:
+
+- [X] pouvoir sauvegarder une partie en cours, et la recharger ensuite
+
+- [ ] pouvoir sauvegarder le score d’une partie (le nombre de déplacements de robots), et pouvoir afficher les meilleurs scores
+
+- [ ] pouvoir revenir en arrière en annulant les derniers déplacements
+
+- [ ] pouvoir éditer un plateau de jeu: placement des robots, de la cible, et des murs, choix de la couleur de la cible
+
+
+## Défit Actuel
+
+- Mettre en place le score et le compteur
+- Amélioration de la fonction interface initial pour que ce dernière correspond plus au design établi
+- Explication du jeu et du programme
+- Ajout des icons et des images
+- Enrichissement du docstring
+
+
+![](robot_ricochet_UI.png)
--- ricochet_Adel_Jonathan_Yassine_Djibril_Mohamed_Yingqin.py
+++ ricochet_Adel_Jonathan_Yassine_Djibril_Mohamed_Yingqin.py
@@ -19,63 +19,88 @@ COUL_FOND = "#ddd0b6"
 COUL_FOND_2 = "#1b1b1b"
 COUL_FOND_3 = "#292929"
 COUL_QUADR = "#99958d"
+ficher_enregistrement_et_chargement = "map.txt"
+bot_controlling = "R"
 COTE = 47
 charger = False
-
-liste_obstacles = ["2", "3", "R", "B", "Y"]
-bot_controlling = "G"
-
-map_initial = [
-    (7, 13),
-    (5, 0),
-    (4, 6),
-    (8, 2),
-    ((11, 1), "Y"),
-    [
-        "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
-        "201010301010101010101010301010102",
-        "*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*1*",
-        "201010101030101010101010103010102",
-        "*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*1*",
-        "201010101010101030103010101010102",
-        "*1*1*1*1*1*1*1*3*1*3*1*1*1*1*1*1*",
-        "201010101010101010101030101010102",
-        "*3*1*1*1*1*1*1*1*1*1*1*3*1*1*1*3*",
-        "2010101030101B1010101010101010102",
-        "*1*1*1*3*1*1*3*1*1*1*1*1*1*1*1*1*",
-        "2G1010101010301010101010101030102",
-        "*1*3*1*1*1*1*1*1*1*1*1*3*1*1*3*1*",
-        "201030101010101010101010301010102",
-        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
-        "201010101010103030301010101R10102",
-        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
-        "20101Y101010103030301010101010102",
-        "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
-        "201010101010101010101010101010102",
-        "*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*3*",
-        "201010101030101010301010101010102",
-        "*1*3*1*1*1*3*1*1*3*1*1*1*1*3*1*1*",
-        "203010101010101010101010103010102",
-        "*1*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*",
-        "201010101010103010101010101010102",
-        "*1*1*1*1*1*1*1*1*1*3*1*1*1*1*1*1*",
-        "201010101010101010103010101010102",
-        "*3*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*",
-        "201010103010101010101010101010302",
-        "*1*1*1*3*1*1*1*1*1*1*1*1*1*1*1*1*",
-        "201010101010301010101010301010102",
-        "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
-    ]
-]
-
-
-objet = map_initial[4]
 success = False
 
-liste = []
-with open("map.txt") as f:
-    for line in f.readlines():
-        liste.append(list(line[:-1]))
+
+def charger_map():
+    global map_initial
+    if charger:
+        try:
+            with open(ficher_enregistrement_et_chargement, "r") as f:
+                map_initial = []
+                nombre_ligne = 0
+                for line in f.readlines():
+                    if nombre_ligne < 5:
+                        sous_map_initial = line[:-1].split(" ")
+                        sous_map_initial = [int(element) for element in sous_map_initial]
+                        map_initial.append(sous_map_initial)
+                    elif nombre_ligne == 4:
+                        sous_map_initial = line[:-1].split(" ")
+                        sous_map_initial = [int(element) for element in sous_map_initial]
+                        map_initial.append(sous_map_initial)
+                    elif nombre_ligne == 5:
+                        map_initial[-1].append(line[:-1])
+                        map_initial.append([])
+                    elif 5 < nombre_ligne < 39:
+                        map_initial[-1].append(line[:-1])
+                    
+                    nombre_ligne += 1
+                map_initial[4] = ((map_initial[4][0], map_initial[4][1]), map_initial[4][2])
+        except:
+            tkinter.messagebox.showerror("error", "Le fichier du chargement n'existe pas")
+        else:
+            tkinter.messagebox.showinfo("sucess", "Le map est chargé")
+            root.destroy()
+            main()
+    else:
+        map_initial = [
+            (0, 0), # red
+            (5, 0), # green
+            (4, 10), # blue
+            (8, 2), # yellow
+            ((11, 1), "Y"),
+            [
+                "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
+                "201010301010101010101010301010102",
+                "*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*1*",
+                "201010101030101010101010103010102",
+                "*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*1*",
+                "201010101010101030103010101010102",
+                "*1*1*1*1*1*1*1*3*1*3*1*1*1*1*1*1*",
+                "201010101010101010101030101010102",
+                "*3*1*1*1*1*1*1*1*1*1*1*3*1*1*1*3*",
+                "201010103010101010101010101010102",
+                "*1*1*1*3*1*1*3*1*1*1*1*1*1*1*1*1*",
+                "201010101010301010101010101030102",
+                "*1*3*1*1*1*1*1*1*1*1*1*3*1*1*3*1*",
+                "201030101010101010101010301010102",
+                "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+                "201010101010103030301010101010102",
+                "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+                "201010101010103030301010101010102",
+                "*1*1*1*1*1*1*1*3*3*1*1*1*1*1*1*1*",
+                "201010101010101010101010101010102",
+                "*1*1*1*1*1*1*1*1*1*1*1*1*1*1*1*3*",
+                "201010101030101010301010101010102",
+                "*1*3*1*1*1*3*1*1*3*1*1*1*1*3*1*1*",
+                "203010101010101010101010103010102",
+                "*1*1*1*1*1*1*3*1*1*1*1*1*1*1*1*1*",
+                "201010101010103010101010101010102",
+                "*1*1*1*1*1*1*1*1*1*3*1*1*1*1*1*1*",
+                "201010101010101010103010101010102",
+                "*3*1*1*1*1*1*1*1*1*1*1*1*1*1*3*1*",
+                "201010103010101010101010101010302",
+                "*1*1*1*3*1*1*1*1*1*1*1*1*1*1*1*1*",
+                "201010101010301010101010301010102",
+                "*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*",
+            ],
+            30,
+            7
+        ]
 
 
 def creer_objet(position, color):
@@ -96,7 +121,6 @@ def success_or_not(info_bot_controlled):
         success = True
         canvas.unbind_all("<Key>")
         tkinter.messagebox.showinfo("success", "SUccess unlocked")
-        tkinter.messagebox.askquestion("Restart Request", 'Do you wanna restart?')  
 
 
 def dessiner_obstacles():
@@ -115,13 +139,6 @@ def dessiner_obstacles():
                     canvas.create_rectangle(
                         41 + j//2 * 47, 37 + i//2 * 47, 41 + (j//2 + 1) * 47, 45 + i//2 * 47, fill='black'
                     )
-    
-
-def affiche_liste(liste): # for test
-    for sous_liste in liste:
-        for element in sous_liste:
-            print(element, end=" ")
-        print()
 
 
 def quadrillage():
@@ -146,7 +163,6 @@ def deplacement_possible(x, y):
     while liste[index-1][y] not in liste_obstacles:
         up += 1
         index -= 1
-        print(liste[index-1][y])
     index = x
     while liste[index+1][y] not in liste_obstacles:
         down += 1
@@ -163,7 +179,18 @@ def deplacement_possible(x, y):
 
 
 def create_rectangle_en_fonction_de_position(position, color):
-    return canvas.create_oval(48 + COTE * position[1], 48 + COTE * position[0], 80 + COTE * position[1], 80 + COTE * position[0], fill=color)
+    global liste
+    bot_create = canvas.create_oval(48 + COTE * position[1], 48 + COTE * position[0], 80 + COTE * position[1], 80 + COTE * position[0], fill=color)
+    if color == "red":
+        color = "R"
+    elif color == "green":
+        color = "G"
+    elif color == "blue":
+        color = "B"
+    elif color == "yellow":
+        color = "Y"
+    liste[position[0]*2+1][position[1]*2+1] = color
+    return bot_create
 
 
 def renouveler_position_dans_liste():
@@ -171,54 +198,30 @@ def renouveler_position_dans_liste():
     global liste
     if bot_controlling == 'R':
         global info_bot_red
-
         x, y = info_bot_red[0]
-
         info_bot_red = calculer_position(info_bot_red[1][0], info_bot_red[1][1]) + [info_bot_red[-1]]
-        print(info_bot_red)
-        
-        liste[info_bot_red[0][0]][info_bot_red[0][1]] = liste[x][y]
-        liste[x][y] = 0
-        affiche_liste(liste)
+        liste[info_bot_red[0][0]][info_bot_red[0][1]], liste[x][y] = liste[x][y], 0
         return info_bot_red
 
     if bot_controlling == 'G':
         global info_bot_green
-
         x, y = info_bot_green[0]
-
         info_bot_green = calculer_position(info_bot_green[1][0], info_bot_green[1][1]) + [info_bot_green[-1]]
-        print(info_bot_green)
-        
-        liste[info_bot_green[0][0]][info_bot_green[0][1]] = liste[x][y]
-        liste[x][y] = 0
-        affiche_liste(liste)
+        liste[info_bot_green[0][0]][info_bot_green[0][1]], liste[x][y] = liste[x][y], 0
         return info_bot_green
 
     if bot_controlling == 'B':
         global info_bot_blue
-
         x, y = info_bot_blue[0]
-
         info_bot_blue = calculer_position(info_bot_blue[1][0], info_bot_blue[1][1]) + [info_bot_blue[-1]]
-        print(info_bot_blue)
-        
-        liste[info_bot_blue[0][0]][info_bot_blue[0][1]] = liste[x][y]
-        liste[x][y] = 0
-        affiche_liste(liste)
+        liste[info_bot_blue[0][0]][info_bot_blue[0][1]], liste[x][y] = liste[x][y], 0
         return info_bot_blue
     
     if bot_controlling == 'Y':
         global info_bot_yellow
-
         x, y = info_bot_yellow[0]
-
         info_bot_yellow = calculer_position(info_bot_yellow[1][0], info_bot_yellow[1][1]) + [info_bot_yellow[-1]]
-        print(info_bot_yellow)
-        
-        liste[info_bot_yellow[0][0]][info_bot_yellow[0][1]] = liste[x][y]
-        liste[x][y] = 0
-        affiche_liste(liste)
+        liste[info_bot_yellow[0][0]][info_bot_yellow[0][1]], liste[x][y] = liste[x][y], 0
         return info_bot_yellow
     
 
@@ -283,18 +286,69 @@ def deplacement(event):
             success_or_not(info_bot_yellow)
 
     elif key == "g":
-        bot_controlling = "G"
-        liste_obstacles = ["2", "3", "R", "B", "Y"]
+        change_bot_controlling("G")
     elif key == "r":
-        bot_controlling = "R"
-        liste_obstacles = ["2", "3", "G", "B", "Y"]
+        change_bot_controlling("R")
     elif key == 'b':
-        bot_controlling = "B"
-        liste_obstacles = ["2", "3", "G", "R", "Y"]
+        change_bot_controlling("B")
     elif key == 'y':
-        bot_controlling = "Y"
-        liste_obstacles = ["2", "3", "G", "R", "B"]
-    
+        change_bot_controlling("Y")
+
+
+def change_bot_controlling(color_abrev):
+    global liste_obstacles, bot_controlling
+    bot_controlling = color_abrev
+    liste_obstacles = ["2", "3", "R", "B", "Y", "G"]
+    liste_obstacles.remove(color_abrev)
+
+
+def map_enregistrement():
+    try:
+        with open(ficher_enregistrement_et_chargement, "w") as f:
+            f.write(" ".join([str(element) for element in list(info_bot_red[1])]) + "\n")
+            f.write(" ".join([str(element) for element in list(info_bot_green[1])]) + "\n")
+            f.write(" ".join([str(element) for element in list(info_bot_blue[1])]) + "\n")
+            f.write(" ".join([str(element) for element in list(info_bot_yellow[1])]) + "\n")
+            f.write(" ".join([str(element) for element in list(objet[0])]) + "\n")
+            f.write(objet[1] + "\n")
+            for line in map_initial[5]:
+                f.write(line + "\n")
+    except:
+        tkinter.messagebox.showerror("error", "Le map ne peut pas être enregistrer")
+    else:
+        tkinter.messagebox.showinfo("success", "Le map est enregistré")
+
+
+def charger_map_depuis_un_fichier():
+    global charger
+    charger = True
+    charger_map()
+    charger = False
+
+def recommencer():
+    root.destroy()
+    main()
+
+def clique_du_souris(event):
+    global charger
+    if (366 < event.x < 468 and 370 < event.y < 464 or
+        370 < event.x < 464 and 366 < event.y < 468):
+        recommencer()
+    elif 830 < event.x < 925 and 340 < event.y < 410:
+        map_enregistrement()
+    elif 947 < event.x < 1043 and 340 < event.y < 410:
+        charger_map_depuis_un_fichier()
+    elif 41 < event.x < 793 or 41 < event.x < 793:
+        x, y = event.y//47-1, event.x//47-1
+        if x == info_bot_red[1][0] and y == info_bot_red[1][1]:
+            change_bot_controlling("R")
+        elif x == info_bot_green[1][0] and y == info_bot_green[1][1]:
+            change_bot_controlling("G")
+        elif x == info_bot_blue[1][0] and y == info_bot_blue[1][1]:
+            change_bot_controlling("B")
+        elif x == info_bot_yellow[1][0] and y == info_bot_yellow[1][1]:
+            change_bot_controlling("Y")
+
 
 def mouvement_up(bot_controlled, info_bot_controlled):
     global success
@@ -333,20 +387,31 @@ def mouvement_right(bot_controlled, info_bot_controlled):
     return info_bot_controlled
 
 
-def charger_map():
-    pass
+def charger_bot_et_objet_et_liste():
+    global info_bot_red, info_bot_green, info_bot_blue, info_bot_yellow
+    global bot_red, bot_green, bot_blue, bot_yellow
+    global objet, liste
+    
+    liste = []
+    for element in map_initial[5]:
+        liste.append(list(element))
+
+    info_bot_red = calculer_position(*map_initial[0]) + ["red"]
+    info_bot_green = calculer_position(*map_initial[1]) + ["green"]
+    info_bot_blue = calculer_position(*map_initial[2]) + ["blue"]
+    info_bot_yellow = calculer_position(*map_initial[3]) + ["green"]
+
+    bot_red = create_rectangle_en_fonction_de_position(info_bot_red[1], "red")
+    bot_green = create_rectangle_en_fonction_de_position(info_bot_green[1], "green")
+    bot_blue = create_rectangle_en_fonction_de_position(info_bot_blue[1], "blue")
+    bot_yellow = create_rectangle_en_fonction_de_position(info_bot_yellow[1], "yellow")
+
+    objet = map_initial[4]
+    creer_objet(*objet)
 
 
-def init():
-    global root, canvas
-    root = tk.Tk()
-    root.title('Robot Ricochet')
-    root.geometry(f"{LARGEUR+10}x{HAUTEUR+10}")
-    root.configure(bg=COUL_FOND_2)
-    root.resizable(False, False)
+def interface_initial():
 
-    canvas = tk.Canvas(height=HAUTEUR, width=LARGEUR, bg=COUL_FOND_2)
-    canvas.grid()
     canvas.create_rectangle(33, 33, 801, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
     canvas.create_rectangle(41, 41, 793, 793, fill=COUL_FOND)
     quadrillage()
@@ -357,60 +422,64 @@ def init():
     canvas.create_rectangle(1065, 340, 1160, 410, fill=COUL_FOND_3, outline=COUL_FOND_3)
     canvas.create_rectangle(830, 441, 1160, 801, fill=COUL_FOND_3, outline=COUL_FOND_3)
 
-        ##rond de couleur
-    canvas.create_oval(864, 717, 914, 767, fill= 'green')
-    canvas.create_oval(864, 647, 914, 697, fill= 'blue')
-    canvas.create_oval(1001, 717, 1051, 767, fill= 'yellow')
-    canvas.create_oval(1001, 647, 1051, 697, fill= 'red')
+    ##rond de couleur
+    canvas.create_oval(864, 717, 914, 767, fill='green')
+    canvas.create_oval(864, 647, 914, 697, fill='blue')
+    canvas.create_oval(1001, 717, 1051, 767, fill='yellow')
+    canvas.create_oval(1001, 647, 1051, 697, fill='red')
 
     ##Lettres
-    canvas.create_text(889, 742, fill= 'white', text='G',font = 'ArcadeClassic 30')
-    canvas.create_text(889, 672, fill= 'white', text='B',font = 'ArcadeClassic 30')
-    canvas.create_text(1026, 742, fill= '#9FAB18', text='Y',font = 'ArcadeClassic 30')
-    canvas.create_text(1026, 672, fill= 'white', text='R',font = 'ArcadeClassic 30')
+    canvas.create_text(889, 742, fill='white', text='G',font='ArcadeClassic 30')
+    canvas.create_text(889, 672, fill='white', text='B',font='ArcadeClassic 30')
+    canvas.create_text(1026, 742, fill='#9FAB18', text='Y',font='ArcadeClassic 30')
+    canvas.create_text(1026, 672, fill='white', text='R',font='ArcadeClassic 30')
 
     ##Texte des couleur
-    canvas.create_text(949, 742, fill= 'white', text='Green',font = 'ArcadeClassic 17')
-    canvas.create_text(949, 672, fill= 'white', text='Blue',font = 'ArcadeClassic 17')
-    canvas.create_text(1086, 742, fill= 'white', text='Yellow',font = 'ArcadeClassic 17')
-    canvas.create_text(1086, 672, fill= 'white', text='Red',font = 'ArcadeClassic 17')
+    canvas.create_text(949, 742, fill='white', text='Green',font='ArcadeClassic 17')
+    canvas.create_text(949, 672, fill='white', text='Blue',font='ArcadeClassic 17')
+    canvas.create_text(1086, 742, fill='white', text='Yellow',font='ArcadeClassic 17')
+    canvas.create_text(1086, 672, fill='white', text='Red',font='ArcadeClassic 17')
 
     ##rectangle orange
     canvas.create_rectangle(1001, 57.5, 1125, 117.5, fill="orange", outline='yellow')
     canvas.create_rectangle(1001, 140, 1125, 200, fill='orange', outline='yellow')
     canvas.create_rectangle(1001, 222.5, 1125, 282.5, fill='orange', outline='yellow')
 
-
     ##Instruction
-    canvas.create_text(940, 595, fill= 'white', text='SWITCH ROBOT',font = 'ArcadeClassic 20')
-    canvas.create_text(935, 470, fill= 'white', text='HOW TO MOVE',font = 'ArcadeClassic 20')
+    canvas.create_text(940, 595, fill='white', text='SWITCH ROBOT',font='ArcadeClassic 20')
+    canvas.create_text(935, 470, fill='white', text='HOW TO MOVE',font='ArcadeClassic 20')
     #record
-    canvas.create_text(915, 85, fill= 'white', text='RECORD:',font = 'ArcadeClassic 20')
-    canvas.create_text(1062.5, 85, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+    canvas.create_text(915, 85, fill='white', text='RECORD:',font='ArcadeClassic 20')
+    canvas.create_text(1062.5, 85, fill='black', text='NONE',font='ArcadeClassic 30')
     #score
-    canvas.create_text(920, 167.5, fill= 'white', text='SCORE:',font = 'ArcadeClassic 20')
-    canvas.create_text(1062.5, 167.5, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+    canvas.create_text(920, 167.5, fill='white', text='SCORE:',font='ArcadeClassic 20')
+    canvas.create_text(1062.5, 167.5, fill='black', text='NONE',font='ArcadeClassic 30')
     #move
-    canvas.create_text(925, 250, fill= 'white', text='MOVE:',font = 'ArcadeClassic 20')
-    canvas.create_text(1062.5, 250, fill= 'black', text='NONE',font = 'ArcadeClassic 30')
+    canvas.create_text(925, 250, fill='white', text='MOVE:',font='ArcadeClassic 20')
+    canvas.create_text(1062.5, 250, fill='black', text='NONE',font='ArcadeClassic 30')
 
+
+def main():
+    global root, canvas
+    root = tk.Tk()
+    if not charger:
+        charger_map()
+    change_bot_controlling(bot_controlling)
+    root.title('Robot Ricochet')
+    root.geometry(f"{LARGEUR+10}x{HAUTEUR+10}")
+    root.configure(bg=COUL_FOND_2)
+    root.resizable(False, False)
+
+    canvas = tk.Canvas(height=HAUTEUR, width=LARGEUR, bg=COUL_FOND_2)
+    canvas.grid()
+    interface_initial()
+    charger_bot_et_objet_et_liste()
     dessiner_obstacles()
-    creer_objet(*objet)
-
+    
     canvas.bind_all("<Key>", deplacement)
+    canvas.bind('<1>', clique_du_souris)
+
+    root.mainloop()
 
 
-init()
-info_bot_red = calculer_position(*map_initial[0]) + ["red"]
-info_bot_green = calculer_position(*map_initial[1]) + ["green"]
-info_bot_blue = calculer_position(*map_initial[2]) + ["blue"]
-info_bot_yellow = calculer_position(*map_initial[3]) + ["green"]
-
-
-bot_red = create_rectangle_en_fonction_de_position(info_bot_red[1], "red")
-bot_green = create_rectangle_en_fonction_de_position(info_bot_green[1], "green")
-bot_blue = create_rectangle_en_fonction_de_position(info_bot_blue[1], "blue")
-bot_yellow = create_rectangle_en_fonction_de_position(info_bot_yellow[1], "yellow")
-
-
-root.mainloop()
+main()
